24bd60cca8cf951a03721c657c61b3434c4eb0b1
JeanPaulCabrera <cabrerafloresjeanpaul@gmail.com>
2026-05-22 20:12:21 -0400

    fix: env use and database fix

--- Infrastructure/Persistence/Migrations/20260522233555_CreateSuppliersTable.Designer.cs
+++ Infrastructure/Persistence/Migrations/20260522233555_CreateSuppliersTable.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Purchases.Api.Infrastructure.Persistence;
@@ -11,9 +12,11 @@ using Purchases.Api.Infrastructure.Persistence;
 namespace Purchases.Api.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(PurchasesDbContext))]
-    partial class PurchasesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260522233555_CreateSuppliersTable")]
+    partial class CreateSuppliersTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
